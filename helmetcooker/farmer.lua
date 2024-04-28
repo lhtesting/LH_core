bee70d22291e556c038cacac523a1a7815d22e06
lhtesting <167361659+lhtesting@users.noreply.github.com>
2024-04-28 13:22:16 -0400

    Update farmer.lua

--- helmetcooker/farmer.lua
+++ helmetcooker/farmer.lua
@@ -3,6 +3,43 @@ local fileSystem = loadstring(game:HttpGet('https://raw.githubusercontent.com/lh
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
+local players = Players
+local lplr = players.LocalPlayer
+local lastCF, stop, heartbeatConnection
+local function start()
+    heartbeatConnection = game:GetService('RunService').Heartbeat:Connect(function()
+        if stop then
+            return 
+        end 
+        lastCF = lplr.Character:FindFirstChildOfClass('Humanoid').RootPart.CFrame
+    end)
+    lplr.Character:FindFirstChildOfClass('Humanoid').RootPart:GetPropertyChangedSignal('CFrame'):Connect(function()
+        stop = true
+        lplr.Character:FindFirstChildOfClass('Humanoid').RootPart.CFrame = lastCF
+        game:GetService('RunService').Heartbeat:Wait()
+        stop = false
+    end)    
+    lplr.Character:FindFirstChildOfClass('Humanoid').Died:Connect(function()
+        heartbeatConnection:Disconnect()
+    end)
+end
+
+lplr.CharacterAdded:Connect(function(character)
+    repeat 
+        game:GetService('RunService').Heartbeat:Wait() 
+    until character:FindFirstChildOfClass('Humanoid')
+    repeat 
+        game:GetService('RunService').Heartbeat:Wait() 
+    until character:FindFirstChildOfClass('Humanoid').RootPart
+    start()
+end)
+
+lplr.CharacterRemoving:Connect(function()
+    heartbeatConnection:Disconnect()
+end)
+
+start()
+
 local LHFolder = fileSystem.loadStorage("Lookin_Hackable")
 local Helmet_File = fileSystem.loadFile(LHFolder,"helmet.txt")
 return function (HelmetFile)
